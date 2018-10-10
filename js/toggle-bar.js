1284d6d7b8ff00cdd99ba7231b1db75b552c65fe
mpodboreski <m.podboreski@gmail.com>
2018-10-10 11:20:07 -0400

    Added classToggleClose Function to nav bar

--- js/toggle-bar.js
+++ js/toggle-bar.js
@@ -5,3 +5,12 @@ function classToggle() {
 }
 document.querySelector('.nav-bar-toggle')
   .addEventListener('click', classToggle);
+
+  function classToggleClose() {
+    const navs = document.querySelectorAll('.nav-bar-items')
+
+    navs.forEach(nav => nav.classList.toggle('nav-bar-ToggleShow'));
+  }
+
+  document.querySelector('.nav-bar-items')
+  .addEventListener('click', classToggle);
